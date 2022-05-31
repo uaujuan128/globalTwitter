26c341271eeefe42f1189c75cf64dea48c1e468e
Juan Díaz Valle <uaujuan128@gmail.com>
2022-05-31 10:24:07 +0200

    Controllador getTweets

--- index.php
+++ index.php
@@ -5,64 +5,42 @@
     <title>Global Twitter</title>
 
     <script src="js/jquery.min.js"></script>
-    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    <script async src="js/widgets.js" charset="utf-8"></script>
     <link  rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-    <div id="tweet" tweetID="515490786800963584"></div>
-
-    <?php
-    if (!isset($_GET['id'])) {
-        exit;
-    } else {
-        $hashtagId = $_GET['id'];
-    }
-
-    require 'bd/connection.php';
-
-    $sql = "SELECT t1.id_busquedatweets, t1.id_tweet,t2.text,t2.followers_count,t2.tiene_foto,t3.screen_name as screen_name, t3.profile_image_url as profile_image_url, t3.name as user_name
-    FROM `twitter_busquedastweets_tweets` AS t1 
-    INNER JOIN `twitter_tweets` AS t2 ON t1.id_tweet = t2.id_tweet
-    INNER JOIN `twitter_usuarios` AS t3 ON t2.id_user_twitter = t3.id_user_twitter
-    WHERE t1.id_busquedatweets = ? and t1.estado = '1' ORDER BY t2.created_at DESC limit 10";
-
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("i", $hashtagId);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    $tweets = array();
-
-    while ($row = $result->fetch_assoc()) {
-        $id_tweet = $row['id_tweet'];
-        array_push($tweets, $id_tweet);
-    }
-    ?>
+    <div id="tweet"></div>
 
     <script>
-        let tweets = <?php echo json_encode($tweets); ?>;
+        let tweets = <?php include 'controller/getTweets.php' ?>;
         let $tweetTemplate = $('.twitter-tweet');
         let $containter = $('.container');
+        let i = 0;
 
-        for (let i = 0; i < tweets.length; i++) {
-            setTimeout(function () {
-                var tweet = document.getElementById("tweet");
-                var id = tweet.getAttribute(tweets[i]);
+        function setTweet() {
+            if (i < tweets.length) {
+                var tweet = $('#tweet')[0];
+                tweet.innerHTML = '';
 
-                twttr.widgets.createTweet(id, tweet,
+                twttr.widgets.createTweet(tweets[i], tweet,
                     {
-                        conversation : 'none',    // or all
-                        cards        : 'hidden',  // or visible
-                        linkColor    : '#cc0000', // default is blue
-                        theme        : 'light'    // or dark
+                        conversation : 'none',
+                        cards        : 'visible',
+                        linkColor    : '#cc0000',
+                        theme        : 'light'
                     })
-                    .then (function (el) {
 
-                    });
-            }, 1000);
+                i++;
+            } else {
+                location.reload();
+            }
         }
 
+        $(document).ready(function () {
+            setTweet();
+            setInterval(setTweet, 5000);
+        });
     </script>
 </body>
 </html>
